432e2cc7b12534d9a1d1841458439afa0e78102f
Balazs Marton <balazs.marton@prezi.com>
2021-05-16 22:16:40 +0200

    feature: added timer label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import (
     Canvas,
+    Label,
     PhotoImage,
     Tk,
 )
@@ -17,18 +18,23 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def setup_ui():
     window = Tk()
     window.title("Pomodoro")
     window.config(padx=100, pady=50, bg=YELLOW)
+    
+    timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+    timer_label.grid(row=0, column=1)
+    
     canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
     tomato = PhotoImage(file="tomato.png")
     canvas.create_image(100, 112, image=tomato)
     canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-    canvas.pack()
+    canvas.grid(row=1, column=1)
+    
     window.mainloop()
 
 
